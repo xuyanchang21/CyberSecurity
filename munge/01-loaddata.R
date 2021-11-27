ffcc2038412cfe7a8e541328f962078a183a81ee
xuyanchang <X.Chang5@newcastle.ac.uk>
2021-11-27 12:08:25 +0000

    edit munge/01_loaddata.R

--- munge/01-loaddata.R
+++ munge/01-loaddata.R
@@ -12,12 +12,7 @@ yy <- unlist(lapply(csvfiles,strsplit,split = "_"))
 
 xx = yy[seq(1,length(yy),2)]
 
-lapply(xx, strsplit, split='-')
-
-unlist(strsplit(xx[1],split = '-'))
 
 kk = yy[seq(0,length(yy),2)]
 
 type <- data.frame(xx=xx,kk=kk)
-
-spread(type,kk,xx)
